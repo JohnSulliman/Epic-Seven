b286b792c3da474bc86301aad72dcd869286b9bc
Sulliman <johnatantc@hotmail.com>
2021-06-23 16:40:49 -0300

    Ajustes na responsividade do projeto

--- epic-seven/src/App.js
+++ epic-seven/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 //Components
-import {Navbar, Nav, Container, Row, Col} from 'react-bootstrap'
+import {Nav, Navbar, NavDropdown, Container, Row, Col} from 'react-bootstrap'
 import {Switch, Route} from 'react-router-dom'
 import {ReadAll} from './Components/ReadAll/ReadAll'
 import {ReadSingle} from './Components/ReadSingle/ReadSingle'
@@ -17,14 +17,26 @@ import Logo from './Img/Logo.png'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './Styles/App.scss'
 
+
 function App() {
+  
+
   return (
     <>
-      <div className='logoBar' >
-        <img className='epic-seven-logo' src={Logo} alt='epic-seven-logo' />
-      </div>
+      <Navbar className='dropdownMenu' >
+        <NavDropdown title='Menu'>
+            <NavDropdown.Item eventKey="1" href='/'>Início</NavDropdown.Item>
+            <NavDropdown.Item eventKey="2" href='/create'>Criar</NavDropdown.Item>
+            <NavDropdown.Item eventKey="3" href='/delete-all'>Deletar Tudo</NavDropdown.Item>
+            <NavDropdown.Item eventKey="4" href='/about'>Sobre</NavDropdown.Item>
+        </NavDropdown>
+      </Navbar>
 
-      <Navbar className='justify-content-center' expand='sm'>
+        <div className='logoBar' >
+          <img className='epic-seven-logo' src={Logo} alt='epic-seven-logo' />
+        </div>
+
+      <Navbar className='menu justify-content-center' expand='sm'>
         <Nav>
           <Nav.Link id='link-style' href='/'>Início</Nav.Link>
           <Nav.Link id='link-style' href='/create'>Criar</Nav.Link>
